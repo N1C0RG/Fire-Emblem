1745fb9490f955636a28f7ff99f7052b8242fd6e
N1C0RG <nicoaroyog@gmail.com>
2024-10-18 23:11:19 -0300

    cambios a validaciones

--- Fire-Emblem/Encapsulado/CondicionesValidacionEncapsuladas.cs
+++ Fire-Emblem/Encapsulado/CondicionesValidacionEncapsuladas.cs
@@ -1,6 +1,58 @@
-namespace Fire_Emblem.Encapsulado;
-
+namespace FireEmblem.Encapsulado;
+using Fire_Emblem;
 public class CondicionesValidacionEncapsuladas
 {
-    
+    private bool condicionValidacionLargo(Player jugador, Player rival)
+    {
+        bool condicion = jugador.equipo.Count > 3 || jugador.equipo.Count < 1 || rival.equipo.Count > 3 ||
+                         rival.equipo.Count < 1;
+        return condicion; 
+    }
+    private bool validacionRepetidosYCantidadHabilidad(Player jugador) 
+    { 
+        foreach (Personaje personaje in jugador.equipo)
+        {
+            if (condicionCantidadHabilidades(personaje))
+            {
+                return false; 
+            }
+            if (condicionCantidadHabilidadesYRepetidos(personaje))
+            {
+                return false; 
+            }
+        }
+        return true; 
+    }
+    private bool condicionCantidadHabilidades(Personaje jugador)
+    {
+        bool condicion = jugador.habilidades.Length > 2;
+        return condicion; 
+    }
+    private bool condicionCantidadHabilidadesYRepetidos(Personaje jugador)
+    {
+        bool condicion = jugador.habilidades.Length == 2 && jugador.habilidades[0] == jugador.habilidades[1]; 
+        return condicion; 
+    }
+    private bool validacionRepetidosNombre(Player jugador) 
+    {
+        List<string> nombreNoRepetidos = new List<string>(); 
+        
+        foreach (Personaje personaje in jugador.equipo) 
+        {
+            if (nombreNoRepetidos.Contains(personaje.name))
+            {
+                return false; 
+            }
+            nombreNoRepetidos.Add(personaje.name);
+        }
+        return true; 
+    }
+    public bool validarEquipoCompleto(Player jugador, Player rival)
+    {
+        return !condicionValidacionLargo(jugador, rival)
+               && validacionRepetidosYCantidadHabilidad(jugador)
+               && validacionRepetidosYCantidadHabilidad(rival)
+               && validacionRepetidosNombre(jugador)
+               && validacionRepetidosNombre(rival);
+    }
 }
--- Fire-Emblem/Validacion.cs
+++ Fire-Emblem/Validacion.cs
@@ -1,67 +1,21 @@
-namespace Fire_Emblem;
+using FireEmblem.Encapsulado;
 
-using Fire_Emblem_View;
+namespace Fire_Emblem;
 
 public class Validacion
 {
     private Player _jugador;
     private Player _rival;
+    private CondicionesValidacionEncapsuladas _condicion = new CondicionesValidacionEncapsuladas(); //TODO: ver si encapsular la logica tiene sentido 
+
     public Validacion(Player jugador, Player rival)
     {
         _jugador = jugador;
-        _rival = rival; 
+        _rival = rival;
     }
-    private bool validacionLargo()
-    {
-        if (_jugador.equipo.Count > 3 || _jugador.equipo.Count < 1 || _rival.equipo.Count > 3 || _rival.equipo.Count < 1)
-        {
-            return false; 
-        }
-        else
-        {
-            return true; 
-        }
-    }
-    private bool validacionRepetidosYCantidadHabilidad(Player jugador) 
-    { 
-        foreach (Personaje personaje in jugador.equipo)
-        {
-            if (personaje.habilidades.Length > 2)
-            {
-                return false; 
-            }
-            if (personaje.habilidades.Length == 2 && personaje.habilidades[0] == personaje.habilidades[1])
-            {
-                return false; 
-            }
-        }
-        return true; 
-    }
-    
-    private bool validacionRepetidosNombre(Player jugador) 
-    {
-        List<string> nombreNoRepetidos = new List<string>(); 
-        
-        foreach (Personaje personaje in jugador.equipo) 
-        {
-            if (nombreNoRepetidos.Contains(personaje.name))
-            {
-                return false; 
-            }
-            nombreNoRepetidos.Add(personaje.name);
-        }
-        return true; 
-    }
+
     public bool EquipoValido()
     {
-        if (validacionLargo() == false 
-            || validacionRepetidosYCantidadHabilidad(_jugador) == false 
-            || validacionRepetidosYCantidadHabilidad(_rival) == false
-            || validacionRepetidosNombre(_jugador) == false
-            || validacionRepetidosNombre(_rival) == false)
-        {
-            return false; 
-        }
-        return true; 
+        return _condicion.validarEquipoCompleto(_jugador, _rival);
     }
 }
